e4ab18280f746f8c5a5b462f66c0969a836969d8
ruviRedner <r.y.r7966@gmail.com>
2024-09-09 16:05:31 +0300

    creating delete and saving to local storege

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 const submit = document.getElementById("submit")
 const input = document.getElementById("text")
-const h1 = document.createElement("h1")
+const container = document.createElement("div")
 
+submit.addEventListener("click", () => {
 
-submit.addEventListener("click",()=>{
-       
        const t = input.value
        const newDiv = document.createElement("div")
        const newLabal = document.createElement("labal")
@@ -13,8 +12,57 @@ submit.addEventListener("click",()=>{
        newLabal.textContent = t
        newDiv.appendChild(newLabal)
        newDiv.appendChild(chekbox)
-       document.body.appendChild(newDiv)
-       input.value = " "
-      
+       container.appendChild(newDiv)
+       document.body.appendChild(container)
+       input.value = ""
+       saveToLocalstorege()
+       
+       
+
+       chekbox.addEventListener("change", (e) => {
+              if (e.target.checked) {
+                     saveToLocalstorege()
+                     if(!newDiv.querySelector("button")){
+                        const Delete = document.createElement("button")
+                        Delete.textContent = "מחק"
+                        newDiv.appendChild(Delete)
+                        Delete.addEventListener("click",()=>{
+                            newDiv.remove()
+                            saveToLocalstorege()
+                     })
+                     }
+              }
+              else{
+                     const delBtn = newDiv.querySelector("button")
+                     delBtn.remove()
+                     saveToLocalstorege()
+              }
+       })
 })
+function saveToLocalstorege() {
+       const todoList = []
+       const divContainer = container.querySelectorAll("div")
+       divContainer.forEach(div => {
+              const text = div.querySelector("labal").textContent
+              const checked = div.querySelector("input").checked
+              todoList.push({text,checked})      
+       })
+       localStorage.setItem("task",JSON.stringify(todoList))       
+}
+// function loedTodo() {
+//       const to = localStorage.getItem("task")
+//       if(to){
+//        const todo = JSON.parse(to)
+//         todo.forEach(() =>{
+          
+//         })
+//       }
+       
+// }
+
+
+
+
+
+
 
